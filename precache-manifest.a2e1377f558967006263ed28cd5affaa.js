327db9f709f7d763e9e78a4b7c92ee1e7424ad79
Izabela <bebelacampostcms@gmail.com>
2020-09-13 15:39:27 -0300

    Updates

---
+++ precache-manifest.a2e1377f558967006263ed28cd5affaa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d5f331d42863b395dbc1f52a3fb8854",
+    "url": "/copa-front/index.html"
+  },
+  {
+    "revision": "270fd3db90e20584c504",
+    "url": "/copa-front/static/css/main.71a58489.chunk.css"
+  },
+  {
+    "revision": "c9b0496916919f516e75",
+    "url": "/copa-front/static/js/2.089c4689.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/copa-front/static/js/2.089c4689.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "270fd3db90e20584c504",
+    "url": "/copa-front/static/js/main.aef1a70c.chunk.js"
+  },
+  {
+    "revision": "5491ae4d7b7f0d54d328",
+    "url": "/copa-front/static/js/runtime-main.93b36df0.js"
+  },
+  {
+    "revision": "449e2a25a336e59f18a52cc8914e41ba",
+    "url": "/copa-front/static/media/logo.449e2a25.png"
+  }
+]);
